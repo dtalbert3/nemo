779a2a6748ab04916ce543161084f230be526892
codyemoffitt <codyemoffitt@gmail.com>
2016-02-13 20:16:28 -0600

    Now only observation facts for patients in the MySQL NEMO_Datamart are copied

--- data-loader/db_connection.js
+++ data-loader/db_connection.js
@@ -7,7 +7,6 @@ var data = fs.readFileSync('./dataLoaderConfig.json'),
 
 try {
   dataLoaderOptions = JSON.parse(data);
-  // console.dir(dataLoaderOptions);
 } catch (err) {
   console.log('There has been an error parsing your JSON.');
   console.log(err);
@@ -46,14 +45,19 @@ var testPatients = require('./models/NEMO/TestPatients')(dataMartCon);
 
 
 // This function breaks the table into chunks and copies them from the source postgres DB to the target DB
-var copyTable = function(start, chunk, sourceModel, destModel, orderColumn) {
+var copyTable = function(start, chunk, sourceModel, destModel,
+  orderColumn,
+  whereClause, callBackFunction) {
+  callBackFunction = null || callBackFunction;
+  whereClause = null || whereClause;
   sourceModel.findAll({
       offset: start,
       limit: chunk,
       order: [
         [orderColumn, 'DESC']
       ],
-      ignoreDuplicates: true
+      ignoreDuplicates: true,
+      where: whereClause
     })
     .then(function(rows) {
       // Split selection into chunks, and insert them into NEMO Datamart
@@ -63,49 +67,38 @@ var copyTable = function(start, chunk, sourceModel, destModel, orderColumn) {
       var result = rows.map(function(a) {
         return a.dataValues;
       });
-      if (result.length > 0) {
-        destModel.bulkCreate(result).then(copyTable((start + chunk), chunk,
-          sourceModel, destModel, orderColumn)).error(function(err) {
-          //destModel.bulkCreate(result, {updateOnDuplicate: true}).then(copyTable((start + chunk), chunk, sourceModel, destModel)).error(function(err) {
+      console.log('Result length', result.length);
+      var nextCall;
+      if (result.length < chunk) {
+        nextCall = callBackFunction;
+      } else {
+        nextCall = copyTable((start + chunk), chunk,
+          sourceModel, destModel, orderColumn, whereClause,
+          callBackFunction);
+      }
+      destModel.bulkCreate(result, {
+        ignoreDuplicates: true
+      }).then(
+        function() {
+          if (typeof(nextCall) === 'function') {
+            nextCall();
+          }
+        }).error(
+        function(err) {
           console.log(err);
         }).catch(function(reason) {
-          console.log(reason);
-        });
-      }
+        console.log(reason);
+      });
     });
 };
-
-// Copy all the I2B2 tables
-if (dataLoaderOptions.tablesToCopy.patient_dimension) {
-  copyTable(0, 2048, patient_dimension_kumc, patient_dimension_nemo,
-    'patient_num');
-}
-if (dataLoaderOptions.tablesToCopy.observation_fact) {
-  copyTable(0, 2048, observation_fact_kumc, observation_fact_nemo,
-    'encounter_num');
-}
-if (dataLoaderOptions.tablesToCopy.concept_dimension) {
-  copyTable(0, 2048, concept_dimension_kumc, concept_dimension_nemo,
-    'concept_path');
-}
-if (dataLoaderOptions.tablesToCopy.visit_dimension) {
-  copyTable(0, 2048, visit_dimension_kumc, visit_dimension_nemo,
-    'encounter_num');
-}
-if (dataLoaderOptions.tablesToCopy.provider_dimension) {
-  copyTable(0, 2048, provider_dimension_kumc, provider_dimension_nemo,
-    'provider_id');
-}
-if (dataLoaderOptions.tablesToCopy.code_lookup) {
-  copyTable(0, 2048, code_lookup_kumc, code_lookup_nemo, 'table_cd');
-}
 
 var dividePatients = function() {
   patient_dimension_nemo.findAll().then(function(patients) {
     patients.forEach(function(patient, i) {
-      //  console.log(patient.dataValues.patient_num);
+      console.log('Divide patients');
+      console.log(patient.dataValues.patient_num);
       var patientNum = patient.dataValues.patient_num;
-      console.log(patientNum);
+      //console.log(patientNum);
       var call1 = learnerPatients.findOne({
         where: {
           patient_num: patientNum
@@ -120,7 +113,8 @@ var dividePatients = function() {
         //console.log(results);
         var learnerPatientResults = results[0];
         var testPatientsResults = results[1];
-        if (learnerPatientResults === null && testPatientsResults ===
+        if (learnerPatientResults === null &&
+          testPatientsResults ===
           null) {
           if (i % 2 === 0) {
             learnerPatients.upsert({
@@ -138,5 +132,48 @@ var dividePatients = function() {
     });
   });
 };
-dividePatients = dividePatients;
-//dividePatients();
+
+
+// Copy all the I2B2 tables
+
+var copyObservationsForPatients = function() {
+  if (dataLoaderOptions.dividePatients) {
+    dividePatients();
+  }
+  if (dataLoaderOptions.tablesToCopy.observation_fact) {
+    patient_dimension_nemo.findAll().then(function(patients) {
+      var patientNums = patients.map(function(a) {
+        return a.dataValues.patient_num;
+      });
+
+      copyTable(0, 2048, observation_fact_kumc, observation_fact_nemo,
+        'encounter_num', {
+          patient_num: {
+            $in: patientNums
+          }
+        });
+
+    });
+  }
+};
+
+if (dataLoaderOptions.tablesToCopy.patient_dimension) {
+  copyTable(0, 2048, patient_dimension_kumc, patient_dimension_nemo,
+    'patient_num', null, copyObservationsForPatients);
+}
+
+if (dataLoaderOptions.tablesToCopy.concept_dimension) {
+  copyTable(0, 2048, concept_dimension_kumc, concept_dimension_nemo,
+    'concept_path');
+}
+if (dataLoaderOptions.tablesToCopy.visit_dimension) {
+  copyTable(0, 2048, visit_dimension_kumc, visit_dimension_nemo,
+    'encounter_num');
+}
+if (dataLoaderOptions.tablesToCopy.provider_dimension) {
+  copyTable(0, 2048, provider_dimension_kumc, provider_dimension_nemo,
+    'provider_id');
+}
+if (dataLoaderOptions.tablesToCopy.code_lookup) {
+  copyTable(0, 2048, code_lookup_kumc, code_lookup_nemo, 'table_cd');
+}
