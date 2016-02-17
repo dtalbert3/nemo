50eae4ee486ccd8dbf7321351e9321fef05ec991
codyemoffitt <codyemoffitt@gmail.com>
2016-02-17 15:59:49 -0600

    Added more comments to nemoApi.js

--- web-app/nemoApi.js
+++ web-app/nemoApi.js
@@ -1,25 +1,28 @@
 var Sequelize = require('sequelize');
 var config = require('getconfig');
 
-// Create database
+// Create database connection
 var sequelize = new Sequelize(
   config.server.db.name,
   config.server.db.username,
-  config.server.db.password,
-  {
+  config.server.db.password, {
     host: config.server.db.host,
     dialect: config.server.db.dialect,
-    port:    config.server.db.port,
+    port: config.server.db.port,
     logging: (config.server.db.logging) ? console.log : false
   });
 
 // Test connection to database
+// Need to add error logging for this part
+
 sequelize
   .authenticate()
   .then(function() {
-    console.log('[*] Connection to ' + config.server.db.name + ' database established');
-  }, function (err) {
-    console.log('[ ] Unable to connect to ' + config.server.db.name + ' database: ', err);
+    console.log('[*] Connection to ' + config.server.db.name +
+      ' database established');
+  }, function(err) {
+    console.log('[ ] Unable to connect to ' + config.server.db.name +
+      ' database: ', err);
   });
 
 var userModel = require('./client/models/user')(sequelize);
@@ -30,7 +33,9 @@ exports.userService = {
   // We should send back a token to be used upon each request
   find: function(params, callback) {
     sequelize
-      .query('SELECT * FROM User', { model: userModel })
+      .query('SELECT * FROM User', {
+        model: userModel
+      })
       .then(function(data) {
         return callback(null, data);
       }, function(error) {
@@ -67,10 +72,11 @@ exports.dashboardService = {
 
 };
 
+// Definition of parameter type model for ParameterType table in NEMO Datamart
 var parameterTypeModel = require('./models/ParameterType')(sequelize);
 exports.questionParameters = {
 
-  // Fetch parameters allowed to be used by client
+  // Fetch parameters allowed to be used by client from database
   find: function(params, callback) {
     sequelize.transaction(function() {
       return parameterTypeModel.findAll()
@@ -83,8 +89,11 @@ exports.questionParameters = {
   }
 };
 
+// Definition of question type model for QuestionType table in NEMO Datamart
 var questionTypeModel = require('./models/QuestionType')(sequelize);
 exports.questionTypes = {
+
+  // Fetch question types allowed to be used by the client from the database
   find: function(params, callback) {
     sequelize.transaction(function() {
       return questionTypeModel.findAll()
@@ -97,9 +106,12 @@ exports.questionTypes = {
   }
 };
 
+// Definition of question event model for QuestionEvent table in NEMO Datamart
 var questionEventModel = require('./models/QuestionEvent')(sequelize);
 exports.questionEvents = {
   find: function(params, callback) {
+
+    // Fetch question events allowed to be used by the client from the database
     sequelize.transaction(function() {
       return questionEventModel.findAll()
         .then(function(data) {
