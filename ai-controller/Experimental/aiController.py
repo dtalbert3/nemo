55da79565e3d3549d41cc5af4a09575874c37cb8
arpachuilo <andrew.pachuilo@gmail.com>
2016-03-16 16:18:57 -0500

    Refactored config into a useable import

--- ai-controller/Experimental/aiController.py
+++ ai-controller/Experimental/aiController.py
@@ -4,86 +4,34 @@ import Queue
 import MySQLdb
 import MySQLdb.cursors
 import time
-import os
+import config
 
 CONFIG = None
-MAX_QUEUE_SIZE = 10
-MAX_NUM_THREADS = 1
-TIMEOUT = 5 * 60 # In seconds
-
-HOST = None
-USER = None
-PASS = None
-DB = None
-QUEUEDSTATUS = None
-RUNNINGSTATUS = None
-AWAITINGFEEDBACKSTATUS = None
-
-
-semaphore = threading.BoundedSemaphore(MAX_NUM_THREADS)
-queue = Queue.Queue()
-
-def printConfig():
-    global MAX_QUEUE_SIZE, MAX_NUM_THREADS, HOST, PASS, USER, DB
-    print MAX_QUEUE_SIZE, MAX_NUM_THREADS, HOST, PASS, USER, DB
 
 # Load CONFIG file for settings to be used during runtime
-def loadConfig():
+def loadConfig(semaphore):
     global CONFIG
 
-    if os.path.isfile('aiConfig.json'):
-        try:
-            with open('aiConfig.json', 'r') as f:
-                try:
-                    NEW_CONFIG = json.load(f)
-                except ValueError as e:
-                    print "ValError" , e
-                    return
-        except EnvironmentError as e:
-            print "EnvError" , e
-            return
-    else:
-        print "aiconfig.json not found"
-        return
+    NEW_CONFIG = config.load('aiConfig.json')
 
-    try:
-        setConfig(NEW_CONFIG)
+    if config.validate(NEW_CONFIG):
         CONFIG = NEW_CONFIG
-    except KeyError as e:
-        print 'No keyfield for', e
-        if CONFIG != None:
-            print "Reverting to old config"
-            setConfig(CONFIG)
-        else:
-            print "No previous config to restore to."
-
-def setConfig(config):
-    global MAX_QUEUE_SIZE, MAX_NUM_THREADS, HOST, PASS, USER, DB, QUEUEDSTATUS,RUNNINGSTATUS, AWAITINGFEEDBACKSTATUS, semaphore
-    MAX_QUEUE_SIZE = config['MAX_QUEUE_SIZE']
-    MAX_NUM_THREADS = config['MAX_NUM_THREADS']
-    semaphore = threading.BoundedSemaphore(MAX_NUM_THREADS)
-    HOST = config['HOST']
-    USER = config['USER']
-    PASS = config['PASS']
-    DB = config['DB']
-    QUEUEDSTATUS = config['QUEUEDSTATUS']
-    RUNNINGSTATUS =  config['RUNNINGSTATUS']
-    AWAITINGFEEDBACKSTATUS =  config['AWAITINGFEEDBACKSTATUS']
+        CONTROLLER = CONFIG['CONTROLLER']
+        semaphore = threading.BoundedSemaphore(CONTROLLER['MAX_NUM_THREADS'])
 
 # Fetch questions to be worked on by their di
-def fetchQuestions():
-    global MAX_QUEUE_SIZE, HOST, PASS, USER, DB, QUEUEDSTATUS,RUNNINGSTATUS, AWAITINGFEEDBACKSTATUS
-    global queue
-    # try:
-    db = MySQLdb.connect(HOST, USER, PASS, DB)
-    # except
+def fetchQuestions(queue):
+    global CONFIG
+    DATABASE = CONFIG['DATABASE']
+    CONTROLLER = CONFIG['CONTROLLER']
+    db = MySQLdb.connect(DATABASE['HOST'], DATABASE['USER'], DATABASE['PASS'], DATABASE['DB'])
     cursor = db.cursor()
     cursor.execute(
         "SELECT ID " +
         "FROM Question " +
-        "WHERE StatusID = " + str(QUEUEDSTATUS) + " " +
+        "WHERE StatusID = " + str(DATABASE['QUEUED_STATUS']) + " " +
         "ORDER BY DateModified DESC " +
-        "LIMIT " + str(MAX_QUEUE_SIZE))
+        "LIMIT " + str(CONTROLLER['MAX_QUEUE_SIZE']))
     results = cursor.fetchall()
     for row in results:
         queue.put(row[0])
@@ -96,14 +44,19 @@ def worker(id, semaphore):
     semaphore.release()
 
 def main():
+    global CONFIG
 
+    semaphore = threading.BoundedSemaphore(1)
     # Fetch config
-    loadConfig()
+    loadConfig(semaphore)
+    if CONFIG is None:
+        print 'Could not load config file on . . .'
 
+    queue = Queue.Queue()
     # Run indefinitely
     while True:
         while queue.empty():
-            fetchQuestions()
+            fetchQuestions(queue)
         while not queue.empty():
             semaphore.acquire()
             t = threading.Thread(target=worker, args=(queue.get(), semaphore))
