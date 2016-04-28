f9b2d83a0abbe358961b76c5fa7d465b3337987d
arpachuilo <andrew.pachuilo@gmail.com>
2016-04-28 03:44:06 -0500

    Fixed up some css

--- web-app/src/layouts/BaseLayout.js
+++ web-app/src/layouts/BaseLayout.js
@@ -45,7 +45,7 @@ class BaseLayout extends React.Component {
               <NavLink path='/login' label='Login' hidden={!Auth.loggedIn() ? '' : ' hidden'} />
               <NavLink path='/user' label='User' hidden={Auth.loggedIn() ? '' : ' hidden'} />
               <NavLink path='/global' label='Global' hidden={Auth.loggedIn() ? '' : ' hidden'} />
-              <NavLink path='/about' label='About' />
+              <NavLink path='/about' label='About' hidden='' />
             </Nav>
           </Navbar.Collapse>
           <div id='alert'/>
